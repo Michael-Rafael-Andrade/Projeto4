f33fc0723857c53448dba3056c2e0a04608c66ca
Michael-Rafael-Andrade <michaelrafaeldeandrade@gmail.com>
2026-04-19 21:35:53 -0300

    Construindo app.js - Footer, Header, Layout, Home, Section 1
    Construindo app.js - Footer, Header, Layout, Home, Section 1

---
+++ food-app/src/pages/Home/Home.js
@@ -0,0 +1,29 @@
+import Section1 from "./Section1.js";
+import React from "react";
+import Layout from "../../components/Layouts/Layout.js";
+
+function Home(){
+
+    return(
+
+        <Layout>
+            {/* Home Section Banner */}
+            <Section1/>
+
+            {/* Home Section About */}
+
+            {/* Home Section Menu */}
+            
+            {/* Home Section Promotion */}
+            
+            {/* Home Section Shop */}
+            
+            {/* Home Section Blog */}
+            
+            {/* Home Section Contact */}
+
+        </Layout>
+    );
+}
+
+export default Home;
---
+++ food-app/src/pages/Home/Section1.js
@@ -0,0 +1,13 @@
+
+
+function Section1(){
+
+    return(
+
+        <div>
+
+        </div>
+    );
+}
+
+export default Section1;
