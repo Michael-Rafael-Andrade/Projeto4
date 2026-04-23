ac4f1f8e8a8e9dbedcbed51bd3fcba42d1eb1f38
Michael-Rafael-Andrade <michaelrafaeldeandrade@gmail.com>
2026-04-23 19:57:55 -0300

    Update Section2.js

--- food-app/src/pages/Home/Section2.js
+++ food-app/src/pages/Home/Section2.js
@@ -8,19 +8,19 @@ import Delivery from "../../assets/about/delivery-bike.png";
 
 const mockData = [
     {
-    image: Pizza,
-    title: "Sabor Original",
-    paragraph: "Nossas receitas seguem tradições clássicas, utilizando ingredientes frescos e selecionados para garantir aquele gosto autêntico de comida feita na hora."
+        image: Pizza,
+        title: "Sabor Original",
+        paragraph: "Nossas receitas seguem tradições clássicas, utilizando ingredientes frescos e selecionados para garantir aquele gosto autêntico de comida feita na hora."
     },
     {
-    image: Salad,
-    title: "Qualidade Garantida",
-    paragraph: "Trabalhamos com fornecedores locais e produtos orgânicos de alta qualidade, garantindo uma refeição nutritiva, saudável e cheia de sabor."
+        image: Salad,
+        title: "Qualidade Garantida",
+        paragraph: "Trabalhamos com fornecedores locais e produtos orgânicos de alta qualidade, garantindo uma refeição nutritiva, saudável e cheia de sabor."
     },
     {
-    image: Delivery,
-    title: "Entrega Rápida",
-    paragraph: "Sua fome não pode esperar. Temos uma logística inteligente para que seu pedido chegue quente e impecável na sua porta em tempo recorde."
+        image: Delivery,
+        title: "Entrega Rápida",
+        paragraph: "Sua fome não pode esperar. Temos uma logística inteligente para que seu pedido chegue quente e impecável na sua porta em tempo recorde."
     }
 
 ];
@@ -33,7 +33,7 @@ function Section2() {
             <section className="about_section">
                 <Container>
                     <Row>
-                        <Col lg={{span:8, offset:2}} className="text-center">
+                        <Col lg={{ span: 8, offset: 2 }} className="text-center">
                             <h2>O hambúrguer fica mais gostoso quando você come com a sua família</h2>
                             <p>
                                 Acreditamos que o segredo de um bom lanche não está penas nos ingredientes selecionados mas nos momentos compartilhados à mesa. Cada mordida ganha um sabor especial quando cercada por boas risadas e a companhia de quem amamos. Por isso, preparamos nossas receitas com o carinho de quem cozinha para a própria família, transformando uma simples refeição em uma memória inesquecível.
@@ -43,12 +43,67 @@ function Section2() {
                             </Link>
                         </Col>
                     </Row>
-                    <Row>
-                        <Col>
-                        
-                        </Col>
-                    </Row>
                 </Container>
+            </section>
+            <section className="about_wrapper">
+                <container>
+                    <Row className="justify-content-md-center">
+                        {/* Coluna 1: Sabor Original */}
+                        <Col md={6} lg={4} className="mb-4 mb-md-0">
+                            <div className="about_box text-center">
+                                <div className="about_icon">
+                                    <img
+                                        src={Pizza}
+                                        className="img-fluid"
+                                        alt="Sabor Original"
+                                    >
+                                    </img>
+
+                                </div>
+                                <h4>Sabor Original</h4>
+                                <p>Nossas receitas seguem tradições clássicas, utilizando ingredientes frescos e selecionados para garantir aquele gosto autêntico de comida feita na hora.</p>
+                            </div>
+                        </Col>
+
+                        {/* Coluna 2: Qualidade Garantida */}
+                        <Col md={6} lg={4} className="mb-4 mb-md-0">
+                            <div className="about_box text-center">
+                                <div className="about_icon">
+                                    <img
+                                        src={Salad}
+                                        className="img-fluid"
+                                        alt="Salada"
+                                    >
+                                    </img>
+
+                                </div>
+                                <h4>Qualidade Garantida</h4>
+                                <p>trabalhamos com fornecedores locais e produtos orgânicos de alta qualidade, garantindo uma refeição nutritiva, saudável e cheia de sabor.</p>
+                            </div>
+                        </Col>
+
+
+                        {/* Coluna 3: Entrega Rápida */}
+                        <Col md={6} lg={4} className="mb-4 mb-md-0">
+                            <div className="about_box text-center">
+                                <div className="about_icon">
+                                    <img
+                                        src={Delivery}
+                                        className="img-fluid"
+                                        alt="Entrega Rápida"
+                                    >
+                                    </img>
+
+                                </div>
+                                <h4>Entrega Rápida</h4>
+                                <p>Sua fome não pode esperar. Temos uma logística inteligente para que seu pedido chegue quente e impecável na sua porta em tempo recorde.</p>
+                            </div>
+                        </Col>
+
+                    </Row>
+
+                </container>
+
             </section>
         </>
     );
