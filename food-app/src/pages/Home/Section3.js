8e5886e833244b499fe3c0ae2f8cafc2ddfa3275
Michael-Rafael-Andrade <michaelrafaeldeandrade@gmail.com>
2026-04-23 20:37:32 -0300

    Criando a Section3 e o mockData
    Criando a Section3 e o mockData

---
+++ food-app/src/pages/Home/Section3.js
@@ -0,0 +1,90 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Image1 from "../../assets/menu/burguer-11.png";
+import Image1 from "../../assets/menu/burguer-12.png";
+import Image1 from "../../assets/menu/burguer-13.png";
+import Image1 from "../../assets/menu/burguer-14.png";
+import Image1 from "../../assets/menu/burguer-15.png";
+import Image1 from "../../assets/menu/burguer-16.png";
+import Image1 from "../../assets/menu/burguer-17.png";
+import Image1 from "../../assets/menu/burguer-18.png";
+
+import { Link } from "react-router-dom";
+
+const mockData = [
+    {
+        id: "0001",
+        image1: Image1,
+        title: "Crispy chicken",
+        paragraph: "Frango crocante, molho chili, tomates, picles e coleslaw artesanal.",
+        rating: 5,
+        price: 34.90
+    },
+    {
+        id: "0002",
+        image1: Image2,
+        title: "Ultimate Bacon",
+        paragraph: "Blend da casa, queijo cheddar, bacon crocante, cebola e mostarda.",
+        rating: 4.5,
+        price: 38.50
+    },
+    {
+        id: "0003",
+        image1: Image3,
+        title: "Black Sheep",
+        paragraph: "Queijo prato, relish de tomate, abacate fresh, alface e cebola roxa.",
+        rating: 4.5,
+        price: 42.00
+    },
+    {
+        id: "0004",
+        image1: Image4,
+        title: "Vegan Burguer",
+        paragraph: "Hambúrguer de grão-de-bico, queijo vegano, alface, tomate e maionese de ervas.",
+        rating: 4.0,
+        price: 36.90
+    },
+    {
+        id: "0005",
+        image1: Image5,
+        title: "Double Burguer",
+        paragraph: "Dois blends de 160g, dobro de cheddar, picles e molho especial.",
+        rating: 5.0,
+        price: 45.90
+    },
+    {
+        id: "0006",
+        image1: Image6,
+        title: "Turkey Burguer",
+        paragraph: "Hambúrguer de peru suculento, cheddar, cebola caramelizada e alface.",
+        rating: 4.0,
+        price: 32.00
+    },
+    {
+        id: "0007",
+        image1: Image7,
+        title: "Somokey House",
+        paragraph: "Blend defumado, queijo provolone, cebola crispy e molho barbecue.",
+        rating: 4.5,
+        price: 39.90
+    },
+    {
+        id: "0008",
+        image1: Image8,
+        title: "Classic Burguer",
+        paragraph: "O clássico: carne, queijo, ketchup, mostarda, picles e cebola branca.",
+        rating: 4.0,
+        price: 28.50
+    },
+
+
+];
+
+
+
+function Section2(){
+
+
+}
+
+export default Section2;
